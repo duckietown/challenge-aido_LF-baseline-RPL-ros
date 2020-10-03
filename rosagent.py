ce6e98568c20ef74156afe58e0b0af0c43020c34
liampaull <paulll@iro.umontreal.ca>
2020-10-03 14:38:55 -0400

    publishing correct camera info

--- rosagent.py
+++ rosagent.py
@@ -2,6 +2,8 @@ import os
 
 import cv2
 import numpy as np
+import yaml
+import copy
 import rospy
 from duckietown_msgs.msg import WheelsCmdStamped
 from sensor_msgs.msg import CameraInfo, CompressedImage
@@ -29,6 +31,30 @@ class ROSAgent:
         topic = "/{}/camera_node/camera_info".format(self.vehicle)
         self.cam_info_pub = rospy.Publisher(topic, CameraInfo, queue_size=1)
 
+        # copied from camera driver:
+
+
+        # For intrinsic calibration
+        self.cali_file_folder = '/data/config/calibrations/camera_intrinsic/'
+        self.frame_id = rospy.get_namespace().strip('/') + '/camera_optical_frame'
+        self.cali_file = self.cali_file_folder + rospy.get_namespace().strip("/") + ".yaml"
+
+        # Locate calibration yaml file or use the default otherwise
+        if not os.path.isfile(self.cali_file):
+            rospy.logwarn("Calibration not found: %s.\n Using default instead." % self.cali_file)
+            self.cali_file = (self.cali_file_folder + "default.yaml")
+
+        # Shutdown if no calibration file not found
+        if not os.path.isfile(self.cali_file):
+            rospy.signal_shutdown("Found no calibration file ... aborting")
+
+        # Load the calibration file
+        self.original_camera_info = self.load_camera_info(self.cali_file)
+        self.original_camera_info.header.frame_id = self.frame_id
+        self.current_camera_info = copy.deepcopy(self.original_camera_info)
+        rospy.loginfo("Using calibration file: %s" % self.cali_file)
+
+
         # Initializes the node
         rospy.init_node("ROSTemplate")
 
@@ -50,7 +76,10 @@ class ROSAgent:
         """
         Publishes a default CameraInfo - TODO: Fix after distortion applied in simulator
         """
-        self.cam_info_pub.publish(CameraInfo())
+        # Publish the CameraInfo message
+        stamp = rospy.Time.now()
+        self.current_camera_info.header.stamp = stamp
+        self.cam_info_pub.publish(self.current_camera_info)
 
     def _publish_img(self, obs):
         """
@@ -69,3 +98,28 @@ class ROSAgent:
         img_msg.data = np.array(cv2.imencode(".jpg", contig)[1]).tostring()
 
         self.cam_pub.publish(img_msg)
+
+    @staticmethod
+    def load_camera_info(filename):
+        """Loads the camera calibration files.
+
+        Loads the intrinsic and extrinsic camera matrices.
+
+        Args:
+            filename (:obj:`str`): filename of calibration files.
+
+        Returns:
+            :obj:`CameraInfo`: a CameraInfo message object
+
+        """
+        with open(filename, 'r') as stream:
+            calib_data = yaml.load(stream)
+        cam_info = CameraInfo()
+        cam_info.width = calib_data['image_width']
+        cam_info.height = calib_data['image_height']
+        cam_info.K = calib_data['camera_matrix']['data']
+        cam_info.D = calib_data['distortion_coefficients']['data']
+        cam_info.R = calib_data['rectification_matrix']['data']
+        cam_info.P = calib_data['projection_matrix']['data']
+        cam_info.distortion_model = calib_data['distortion_model']
+        return cam_info
