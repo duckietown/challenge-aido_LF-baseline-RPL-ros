db30645314d7b85f41115a37cdcd0506a3c29b59
Liam Paull <paulll@iro.umontreal.ca>
2020-10-07 00:19:26 -0400

    adding launchers

---
+++ launchers/run_and_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /environment.sh
+source /opt/ros/noetic/setup.bash
+source /code/catkin_ws/devel/setup.bash
+source /code/exercise_ws/devel/setup.bash
+python3 solution.py &
+roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME &
+roslaunch --wait duckietown_demos lane_following.launch &
+rostopic pub /$VEHICLE_NAME/fsm_node/mode duckietown_msgs/FSMState '{header: {}, state: "LANE_FOLLOWING"}'
